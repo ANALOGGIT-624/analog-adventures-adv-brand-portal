ad53331bf94b459234f2a856a1c2453f17aec17b
ANALOGGIT-624 <tommy@analogadventureco.com>
2026-09-15 14:07:53 -0400

    Add versioned artwork proof approval workflow

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -19,6 +19,7 @@ export default function App() {
         <s-link href="/app">Dashboard</s-link>
         <s-link href="/app/organizations">Organization stores</s-link>
         <s-link href="/app/campaigns">Campaigns</s-link>
+        <s-link href="/app/proofs">Artwork proofs</s-link>
         <s-link href="/app/attributions">Attributed orders</s-link>
         <s-link href="/app/payouts">Payouts</s-link>
       </s-app-nav>
